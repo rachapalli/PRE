0586a257d7f18df012e0f007ff5534e94a332437
Gaurav Kumar <gaurav.5410@gmail.com>
2017-06-08 17:19:54 +0530

    few lines added

--- opp_pdf/src/main/java/com/precash/opp/server/servlet/TestDemo.java
+++ opp_pdf/src/main/java/com/precash/opp/server/servlet/TestDemo.java
@@ -1,7 +1,9 @@
 package com.precash.opp.server.servlet;
 
 public class TestDemo {
-	public static void main(String[] args) {
-		System.out.println();
-	}
+  public static void main(String[] args) {
+    Integer a = 10;
+    System.out.println("");
+    System.out.println(a);
+  }
 }
